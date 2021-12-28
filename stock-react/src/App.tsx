9054254ee69c24c5fbca5088f548c7c263f298ed
Youn-Young-Seon <szdbsyyszs@naver.com>
2021-12-28 22:55:29 +0900

    stock working...

--- stock-react/src/App.tsx
+++ stock-react/src/App.tsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap'
 import './App.css';
 import './main.scss';
 
-function App() {  
+function App() {
+
+  const [click, clickSet] = useState('addClass');
+
+  function addClass(){
+    clickSet("click");
+  }
 
   return (
     <div className="App">      
       <div id="main-bg">
         <Navbar expand="lg">
-          <Container>
-            <Navbar.Brand href="/" className='main-fontColor'><h3>Stock</h3></Navbar.Brand>
+          <Container className='main-topBar'>
+            <Navbar.Brand href="/" className='main-fontColor--light'><h3>Stock</h3></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                {/* <Nav.Link className='main-fontColor' href="#home">Home</Nav.Link>
-                <Nav.Link className='main-fontColor' href="#link">Link</Nav.Link>                 */}
+                {/* <Nav.Link className='main-fontColor--light' href="#home">Home</Nav.Link>
+                <Nav.Link className='main-fontColor--light' href="#link">Link</Nav.Link>                 */}
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
 
         <div className="row">
-            <div>
-              <h4>안녕</h4>
+            <div className='main-title'>              
+                <h6 className='main-fontColor--light'>Live Rates</h6>
+                <ul className='main-fontColor--blueGreen' onClick={ addClass }>
+                  <li>chap1</li>
+                  <li>chap2</li>
+                  <li>chap3</li>
+                  <li>chap4</li>
+                  <li>chap5</li>
+                </ul>              
             </div>
-            <div className="col-3">
+            {/* <div className="col-3">
               <div className="main-box"></div>
               <div className="main-box"></div>
               <div className="main-box"></div>
@@ -42,7 +55,7 @@ function App() {
             </div>
             <div className="col-3">
               <div className="main-box"></div>              
-            </div>
+            </div> */}
           </div>          
         </div>
     </div>
