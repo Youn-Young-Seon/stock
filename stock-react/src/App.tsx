1cf6d13412f7f69e6eddbc18e08056fa04d10aa2
Youn-Young-Seon <szdbsyyzs@naver.com>
2021-12-29 18:02:25 +0900

    stock css working...

--- stock-react/src/App.tsx
+++ stock-react/src/App.tsx
@@ -1,22 +1,57 @@
-import React, {useState} from 'react';
-import {Navbar, Container, Nav} from 'react-bootstrap'
+import { render } from '@testing-library/react';
+import React, {ReactElement, useState} from 'react';
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import {NavLink, Route} from 'react-router-dom';
+import { JsxElement } from 'typescript';
 import './App.css';
 import './main.scss';
 
 function App() {
 
-  const [click, clickSet] = useState('addClass');
+  let [click, clickSet] = useState('');
+  let [card, cardSet] = useState([0, 1, 2]);
 
-  function addClass(){
-    clickSet("click");
+  function ActiveStyle(){      
+    return(
+      <ul>
+        <NavLink className='main-fontColor--blueGreen' activeClassName={click} exact to='/'>
+          <li onClick={() => { clickSet('active') } }>
+            chap1
+          </li>
+        </NavLink>
+        <NavLink className='main-fontColor--blueGreen' activeClassName={click} to='/path'>
+          <li onClick={ () => { clickSet('active') } }>
+            chap2
+          </li>
+        </NavLink>
+      </ul>
+    )
+  }
+
+  function StockCard(){
+    return(     
+      <>
+        {
+          card.map((a) => {
+            return <div className="col-4">
+                    <div className="main-box"></div>
+                    <div className="main-box"></div>
+                    <div className="main-box"></div>
+                    <div className="main-box"></div>
+                    <div className="main-box"></div>
+                  </div>;
+          })
+        }
+      </>
+    )
   }
 
   return (
     <div className="App">      
       <div id="main-bg">
-        <Navbar expand="lg">
-          <Container className='main-topBar'>
-            <Navbar.Brand href="/" className='main-fontColor--light'><h3>Stock</h3></Navbar.Brand>
+        <Navbar expand="lg" className='header'>
+          <Container className='header-topBar'>
+            <Navbar.Brand href="/" className='main-fontColor--light'><h4>Stock</h4></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
@@ -26,38 +61,81 @@ function App() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-
-        <div className="row">
-            <div className='main-title'>              
-                <h6 className='main-fontColor--light'>Live Rates</h6>
-                <ul className='main-fontColor--blueGreen' onClick={ addClass }>
-                  <li>chap1</li>
-                  <li>chap2</li>
-                  <li>chap3</li>
-                  <li>chap4</li>
-                  <li>chap5</li>
-                </ul>              
+        
+        <div>
+          <div className='section'>
+            <div className="section-left">
+              <div className='section-left-top'>
+                <div className='section-left-top-container'>
+                  <div className="row">
+                    <div className='header-title main-fontColor--light'>
+                      <h6>Live Rates</h6>
+                      <ActiveStyle/>
+                    </div>
+                    <StockCard/>
+                    </div>
+                  </div>
+                </div>
+              <div className='section-left-bottom'>
+                <div className="section-left-bottom-container main-fontColor--light">
+                  <h6>Trades</h6>
+                  <div className="section-left-bottom-container-title">
+                    <table>
+                      <tr>
+                        <th>th</th>
+                        <th>th</th>
+                        <th>th</th>
+                        <th>th</th>
+                        <th>th</th>
+                      </tr>
+                    </table>
+                  </div>
+                  <div className='section-left-bottom-container-table'>
+                    <table>
+                      <tr>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>                                            
+                      </tr>
+                      <tr>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>                                            
+                      </tr>
+                      <tr>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>                                            
+                      </tr>
+                      <tr>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>
+                        <td>td</td>                                            
+                      </tr>
+                    </table>                                    
+                  </div>
+                </div>
+              </div>
             </div>
-            {/* <div className="col-3">
-              <div className="main-box"></div>
-              <div className="main-box"></div>
-              <div className="main-box"></div>
+            <div className='section-right'>
+              <div className="section-right-container"></div>   
             </div>
-            <div className="col-3">
-              <div className="main-box"></div>
-              <div className="main-box"></div>
-              <div className="main-box"></div>
-            </div>
-            <div className="col-3">
-              <div className="main-box"></div>
-              <div className="main-box"></div>
-              <div className="main-box"></div>
-            </div>
-            <div className="col-3">
-              <div className="main-box"></div>              
-            </div> */}
-          </div>          
+          </div>
+          <div className="footer">
+            <div className="footer-container">
+                
+            </div>               
+          </div>
         </div>
+      </div>
     </div>
   );
 }
