6c1f927a9ea7b8923a3c78ffd1cffbdeaf63e3f5
Youn-Young-Seon <szdbsyyzs@naver.com>
2021-12-30 17:29:46 +0900

    stock api parsing...

--- stock-react/src/App.tsx
+++ stock-react/src/App.tsx
@@ -1,21 +1,32 @@
-import { render } from '@testing-library/react';
+import axios from 'axios';
 import React, {ReactElement, useState} from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import {NavLink, Route} from 'react-router-dom';
-import { JsxElement } from 'typescript';
 import './App.css';
 import './main.scss';
+
+const secretKey :string = 'NTVoNqBCa6w4WY0zqtP488WlWdVMsK4H4z%2BanvgdokVDEjJOsFtXTI98sRy2NUrAaw3IUMu0izHR0DFo%2F2XqHA%3D%3D'
 
 function App() {
 
   let [click, clickSet] = useState('');
   let [card, cardSet] = useState([0, 1, 2]);
 
+  function stockInfo(){
+    axios.get(`https://api.odcloud.kr/api/GetStockSecuritiesInfoService/v1/getStockPriceInfo?resultType=json&serviceKey=${secretKey}`)
+    .then((result)=> {
+      console.log(result)
+    })
+    .catch((e)=>{
+
+    })
+  }
+
   function ActiveStyle(){      
     return(
       <ul>
         <NavLink className='main-fontColor--blueGreen' activeClassName={click} exact to='/'>
-          <li onClick={() => { clickSet('active') } }>
+          <li onClick={() => { clickSet('active'); stockInfo();} }>
             chap1
           </li>
         </NavLink>
@@ -36,8 +47,8 @@ function App() {
             return <div className='main-box-row'>
                     <div className="main-box-col"></div>
                     <div className="main-box-col"></div>
-                    <div className="main-box-col"></div>
-                    <div className="main-box-col"></div>                    
+                    <div className="main-box-col"></div>            
+                    <div className="main-box-col"></div>            
                   </div>;
           })
         }
@@ -66,14 +77,12 @@ function App() {
             <div className="section-left">
               <div className='section-left-top'>
                 <div className='section-left-top-container'>
-                  {/* <div className="row"> */}
-                    <div className='main-title main-fontColor--light'>
-                      <h6>Live Rates</h6>
-                      <ActiveStyle/>
-                    </div>
-                    <StockCard/>
-                    </div>
-                  {/* </div> */}
+                  <div className='main-title main-fontColor--light'>
+                    <h6>Live Rates</h6>
+                    <ActiveStyle/>
+                  </div>
+                  <StockCard/>
+                  </div>                
                 </div>
               <div className='section-left-bottom'>
                 <div className="section-left-bottom-container main-fontColor--light">
@@ -138,8 +147,13 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className='section-right'>
-              <div className="section-right-container"></div>   
+            <div className='section-right main-fontColor--light'>              
+              <div className="section-right-title">
+                <h6>Analytics</h6>
+              </div>              
+              <div className="section-right-container">
+                
+              </div>   
             </div>
           </div>
           <div className="footer">
